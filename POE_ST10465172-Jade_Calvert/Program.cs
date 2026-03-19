f95f412518a8055f9f6413e2434a8d38dedef0aa
Noct <zanoftheworld@gmail.com>
2026-03-19 10:26:39 +0200

    Add files via upload

---
+++ POE_ST10465172-Jade_Calvert/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+
+
+
+
+Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+myDictionary.Add("Virus", "A computer virus is a type of malicious software that attaches to files or programs and spreads to other systems causing damage or disruption.");
+myDictionary.Add("Online Safety", "Always create strong passwords that are difficult to guess, ideally combining letters, numbers, and special characters.");
+myDictionary.Add("Key1", "Value1");
+myDictionary.Add("Key1", "Value1");
+myDictionary.Add("Key1", "Value1");
+myDictionary.Add("Key1", "Value1");
